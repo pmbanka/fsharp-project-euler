3f5c3a516406a623f13592dc1a56aa07098cd70e
Paweł Bańka <pawel@pawelbanka.net>
2016-06-26 18:33:28 +0200

    Fix for problem 2

--- src/Euler002.fsx
+++ src/Euler002.fsx
@@ -1,6 +1,6 @@
 let euler2 n =
     let isEven x = x % 2 = 0
-    let fib = Seq.unfold (fun (a, b) -> Some (a+b, (b, a+b))) (0, 1)
+    let fib = Seq.unfold (fun (a, b) -> Some (a+b, (a+b, a))) (0, 1)
     fib 
     |> Seq.where isEven 
     |> Seq.takeWhile (fun x -> x < n) 
