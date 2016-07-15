da0fa8b7284f875cc94f4a7707e0bdac476e309b
Paweł Bańka <pawel@pawelbanka.net>
2016-07-16 00:43:06 +0200

    Forgot to save 37 file :)

--- src/Euler037.fsx
+++ src/Euler037.fsx
@@ -14,8 +14,23 @@ let primesBelow n =
     sieve 2
     table |> Seq.where ((<>) 0) |> Set.ofSeq
 
-let isTruncatable allPrimes prime =
-    
-    ()
+let trunc n = 
+    seq {
+        let str = n.ToString ()
+        for i in 1 .. str.Length-1 do
+            yield str.Substring (i, str.Length - i) 
+        for i in str.Length-1 .. -1 .. 1 do
+            yield str.Substring (0, i) } 
+    |> Seq.map Int32.Parse
 
-primesBelow 1000000
+let isTruncatable allPrimes prime =
+    trunc prime 
+    |> Seq.forall (fun x -> Set.contains x allPrimes)
+
+let all = primesBelow 1000000
+all
+|> Seq.skip 4
+|> Seq.where (isTruncatable all)
+|> Seq.take 11
+|> Seq.sum
+
